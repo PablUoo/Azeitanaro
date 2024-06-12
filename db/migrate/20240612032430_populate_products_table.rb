16af4acb92d6415d370338db184feefe76785b97
pablo <pabloaurelio1163@gmail.com>
2024-06-12 19:20:06 -0300

    Add: rollback no PopulateProductsTable

--- db/migrate/20240612032430_populate_products_table.rb
+++ db/migrate/20240612032430_populate_products_table.rb
@@ -2,4 +2,8 @@ class PopulateProductsTable < ActiveRecord::Migration[6.1]
   def change
     Produto.criar_produtos_do_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTf1kor50Gz5a3l69hScqrLaMITLZrUdShP9W4Qo8zr6A1LM99JYuRLS_jy20tGO9WYoOcJcct7qU8A/pub?gid=1354510115&single=true&output=csv","link")
   end
+
+  def up
+    Produto.all.destroy_all
+  end
 end
